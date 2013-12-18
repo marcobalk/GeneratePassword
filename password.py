5c2603b0996de4488288868280128819152ef8d7
Bibhas <iambibhas@gmail.com>
2013-12-18 21:06:10 +0530

    added more password options, with special characters

--- password.py
+++ password.py
@@ -2,29 +2,31 @@ import sublime, sublime_plugin, string
 from random import sample, choice, randrange
 
 class PasswordCommand(sublime_plugin.TextCommand):
-    chars = "23456789abcdefghijkmnpqrstuvwxyzABCDEFGHKMNPQRSTUVWXYZ"
+    chars = string.ascii_letters + string.digits
+    length = randrange(6, 31)
     
     def run(self, edit):
-        p = ''.join(sample(self.chars, self.length()))
+        p = ''.join(sample(self.chars, self.length))
         for region in self.view.sel():
             self.view.replace(edit, region, p)
 
-    def length(self):
-        return randrange(6, 31)
-
 class GenerateShortPasswordCommand(PasswordCommand):
-    def length(self):
-        return randrange(6, 9)
+    length = 10
 
 class GenerateMediumPasswordCommand(PasswordCommand):
-    def length(self):
-        return randrange(9, 14)
+    length = 15
 
 class GenerateLongPasswordCommand(PasswordCommand):
-    def length(self):
-        return randrange(14, 20)
+    length = 20
 
-class GenerateSecurePasswordCommand(PasswordCommand):
-    chars = string.ascii_letters + string.digits
-    def length(self):
-        return randrange(20, 31)
+class GenerateShortSecurePasswordCommand(PasswordCommand):
+    chars = string.ascii_letters + string.digits + "!@#$%^&*_-+=|/?:;<>~"
+    length = 10
+
+class GenerateMediumSecurePasswordCommand(PasswordCommand):
+    chars = string.ascii_letters + string.digits + "!@#$%^&*_-+=|/?:;<>~"
+    length = 15
+    
+class GenerateLongSecurePasswordCommand(PasswordCommand):
+    chars = string.ascii_letters + string.digits + "!@#$%^&*_-+=|/?:;<>~"
+    length = 20
