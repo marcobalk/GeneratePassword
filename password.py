65be9fa3b8b4ab10fab311ce0fe94bb1002cccd7
Bibhas <iambibhas@gmail.com>
2013-12-18 21:22:58 +0530

    reduced redundant lines

--- password.py
+++ password.py
@@ -2,11 +2,14 @@ import sublime, sublime_plugin, string
 from random import sample, choice, randrange
 
 class PasswordCommand(sublime_plugin.TextCommand):
+    secure = False
     chars = string.ascii_letters + string.digits
+    secure_chars = chars + "!@#$%^&*_-+=|/?:;<>~"
     length = randrange(6, 31)
     
     def run(self, edit):
-        p = ''.join(sample(self.chars, self.length))
+        population = self.secure_chars if self.secure else self.chars
+        p = ''.join(sample(population, self.length))
         for region in self.view.sel():
             self.view.replace(edit, region, p)
 
@@ -19,14 +22,11 @@ class GenerateMediumPasswordCommand(PasswordCommand):
 class GenerateLongPasswordCommand(PasswordCommand):
     length = 20
 
-class GenerateShortSecurePasswordCommand(PasswordCommand):
-    chars = string.ascii_letters + string.digits + "!@#$%^&*_-+=|/?:;<>~"
-    length = 10
+class GenerateShortSecurePasswordCommand(GenerateShortPasswordCommand):
+    secure = True
 
-class GenerateMediumSecurePasswordCommand(PasswordCommand):
-    chars = string.ascii_letters + string.digits + "!@#$%^&*_-+=|/?:;<>~"
-    length = 15
+class GenerateMediumSecurePasswordCommand(GenerateMediumPasswordCommand):
+    secure = True
     
-class GenerateLongSecurePasswordCommand(PasswordCommand):
-    chars = string.ascii_letters + string.digits + "!@#$%^&*_-+=|/?:;<>~"
-    length = 20
+class GenerateLongSecurePasswordCommand(GenerateLongPasswordCommand):
+    secure = True
